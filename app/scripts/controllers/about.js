cf7804297a466a2a6fae2923bd3dc110e6865c9d
Dustyn Tubbs <DustynTubbs@Gmail.com>
2016-12-09 12:19:42 -0600

    Initial Commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name flexWebApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the flexWebApp
+ */
+angular.module('flexWebApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name flexWebApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the flexWebApp
+ */
+angular.module('flexWebApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
